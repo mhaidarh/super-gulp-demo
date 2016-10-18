f7097bdb1546ea1fc336951509aa4773f6b6c537
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-18 21:17:26 +0700

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const gutil = require('gulp-util')
 const standard = require('gulp-standard')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
+const browserSync = require('browser-sync').create()
 
 // define the default task with log message
 gulp.task('default', ['copy-html', 'standard', 'build-sass', 'watch'], function () {
@@ -13,13 +14,13 @@ gulp.task('default', ['copy-html', 'standard', 'build-sass', 'watch'], function 
 
 // copy any html files in src/ to _dist/
 gulp.task('copy-html', function () {
-  gutil.log('[info]'.yellow, 'Copy HTML to _dist/...')
-  gulp.src('src/*.html').pipe(gulp.dest('_dist'));
-});
+  gutil.log('[process]'.yellow, 'Copy HTML to _dist/...')
+  gulp.src('src/*.html').pipe(gulp.dest('_dist'))
+})
 
 // configure the standard.js task
 gulp.task('standard', function () {
-  gutil.log('[info]'.yellow, 'Check standard...')
+  gutil.log('[process]'.yellow, 'Check standard...')
   return gulp.src(['src/scripts/*.js'])
     .pipe(standard())
     .pipe(standard.reporter('default', {
@@ -30,29 +31,36 @@ gulp.task('standard', function () {
 
 // concatenate javascript files
 gulp.task('build-js', function () {
+  gutil.log('[process]'.yellow, 'Concatenate scripts...')
   return gulp.src('src/scripts/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('bundle.js'))
     // only uglify if gulp is ran with '--type production'
     .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('_dist/assets/scripts'));
-});
+    .pipe(gulp.dest('_dist/assets/scripts'))
+})
 
 // sass to css
 gulp.task('build-sass', function () {
-  gutil.log('[info]'.yellow, 'Build Sass to CSS...')
+  gutil.log('[info]'.yellow, 'Compile Sass to CSS...')
   return gulp.src('src/styles/*.sass')
     .pipe(sourcemaps.init()) // Process the original sources
     .pipe(sass())
     .pipe(sourcemaps.write()) // Add the map to modified source.
     .pipe(gulp.dest('_dist/assets/styles'))
+    .pipe(browserSync.stream())
 })
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function () {
-  gutil.log('[info]'.green, 'Watching some files...')
-  gulp.watch('src/*.html', ['copy-html'])
+  gutil.log('[info]'.blue, 'Serving and watching some files...')
+  browserSync.init({
+    server: {
+      baseDir: "_dist"
+    }
+  })
+  gulp.watch('src/*.html', ['copy-html']).on('change', browserSync.reload)
   gulp.watch('src/scripts/*.js', ['standard'])
   gulp.watch('src/styles/*.scss', ['build-sass'])
 })
