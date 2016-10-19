30cdd3b1276f61d461eab260afa72f669eab88e5
M Haidar Hanif <git@mhaidarhanif.com>
2016-10-19 15:46:00 +0700

    test: eating + drinking: separated

--- scripts/human.test.js
+++ scripts/human.test.js
@@ -9,18 +9,23 @@ describe('Array', function () {
 })
 
 describe('Human', function () {
-  it('expect human.eat() to return string', function () {
-    expect(human.eat()).to.be.a('string');
+  describe('Human eating', function () {
+    it('expect human.eat() to return string', function () {
+      expect(human.eat()).to.be.a('string');
+    })
+    it('expect human to eat and gain energy', function () {
+      expect(human.eat(1)).to.contain('Eat')
+      expect(human.eat(1)).to.contain('Energy')
+    })
   })
-  it('expect human to eat and gain energy', function () {
-    expect(human.eat(1)).to.contain('Eat')
-    expect(human.eat(1)).to.contain('Energy')
-  })
-  it('expect human.drink() to return string', function () {
-    expect(human.drink()).to.be.a('string');
-  })
-  it('expect human to eat and gain hidration', function () {
-    expect(human.drink(1)).to.contain('Drink')
-    expect(human.drink(1)).to.contain('Hidration')
+
+  describe('Human drinking', function () {
+    it('expect human.drink() to return string', function () {
+      expect(human.drink()).to.be.a('string');
+    })
+    it('expect human to eat and gain hidration', function () {
+      expect(human.drink(1)).to.contain('Drink')
+      expect(human.drink(1)).to.contain('Hidration')
+    })
   })
 })
